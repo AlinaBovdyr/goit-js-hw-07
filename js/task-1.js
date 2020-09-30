ecc47b09bbeb868739d58f04751a2c1bc72d27af
Alina Bovdyr <alinabovdyr92@gmail.com>
2020-09-30 20:33:07 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -3,10 +3,9 @@ const liElQuantity = liEls.length;
 
 console.log(`В списке ${liElQuantity} категории.`);
 
-for (const liEl of liEls) {
-  const childrenEl = liEl.children;
-  const titleEl = childrenEl[0];
-  const quantityOfEl = childrenEl[1].children.length;
+liEls.forEach(({ children }) => {
+  const titleEl = children[0];
+  const quantityOfEl = children[1].children.length;
   console.log(`Категория: ${titleEl.textContent}`);
   console.log(`Количество элементов: ${quantityOfEl}`);
-}
+});

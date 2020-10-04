46997a3107e3de8bf7b56329c95c43a7d9e75a09
Alina Bovdyr <alinabovdyr92@gmail.com>
2020-10-04 23:30:19 +0300

    Добавляет решение 8 задачи

--- js/task-8.js
+++ js/task-8.js
@@ -14,6 +14,7 @@ function createBoxes(amount) {
   const boxesEl = arr.map(el => {
     const boxEl = document.createElement('div');
     boxEl.textContent = el;
+
     return boxEl;
   });
 
@@ -21,20 +22,44 @@ function createBoxes(amount) {
 }
 
 function onInputChange(event) {
-  // console.log(event.currentTarget.value);
+  console.log(event.currentTarget.value);
 
-  const amount = Number(event.currentTarget.value);
+  // const amount = Number(event.currentTarget.value);
 
-  console.log(createBoxes(amount));
+  // console.log(createBoxes(amount));
+}
+
+function getRandomColor() {
+  var color = '';
+  for (var i = 0; i < 3; i++) {
+    var sub = Math.floor(Math.random() * 256).toString(16);
+    color += sub.length == 1 ? '0' + sub : sub;
+  }
+  return '#' + color;
 }
 
 function onRenderBtnClick() {
   console.log(refs.input.value);
   refs.divBoxes.append(...createBoxes(Number(refs.input.value)));
   console.log(refs.divBoxes);
+
+  const createdDivBoxes = refs.divBoxes.children;
+  console.log(createdDivBoxes);
+
+  for (let i = 0; i < createdDivBoxes.length; i += 1) {
+    const boxWidth = 30;
+    const boxHeight = 30;
+    createdDivBoxes[i].style.backgroundColor = getRandomColor();
+    createdDivBoxes[i].style.width = `${boxWidth + 10}px`;
+    createdDivBoxes[i].style.height = `${boxHeight + 10}px`;
+  }
+
   return refs.divBoxes;
 }
 
 function onDestroyBtnClick() {
   console.log('нажали кнопку Очистить');
+  refs.divBoxes.innerHTML = '';
+  console.log(refs.divBoxes);
+  return refs.divBoxes;
 }
